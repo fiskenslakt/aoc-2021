1dc98beec7b2781886c7b0555adf698db7bdadbe
Derek Silbert <sjukledighet@gmail.com>
2021-12-08 04:51:12 -0500

    Day 8 solution

--- p8.py
+++ p8.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from functools import reduce
+
 from aocd import lines, submit
 
 #    0:      1:      2:      3:      4:
@@ -19,91 +20,75 @@ from aocd import lines, submit
 # .    f  e    f  .    f  e    f  .    f
 #  gggg    gggg    ....    gggg    gggg
 
-#   1:2    4:4
-#  ....   ....
-# .    c b    c
-# .    c b    c
-#  ....   dddd
-# .    f .    f
-# .    f .    f
-#  ....   ....
-
-#   7:3     8:7
-#  aaaa    aaaa
-# .    c  b    c
-# .    c  b    c
-#  ....    dddd
-# .    f  e    f
-# .    f  e    f
-#  ....    gggg
-
-#   0:6    2:5     3:5
-#  aaaa   aaaa    aaaa
-# b    c .    c  .    c
-# b    c .    c  .    c
-#  ....   dddd    dddd
-# e    f e    .  .    f
-# e    f e    .  .    f
-#  gggg   gggg    gggg
-
-#   5:5     6:6    9:6
-#  aaaa    aaaa   aaaa
-# b    .  b    . b    c
-# b    .  b    . b    c
-#  dddd    dddd   dddd
-# .    f  e    f .    f
-# .    f  e    f .    f
-#  gggg    gggg   gggg
-
-
-#
-# a - 8 | 6 | 5
-# b - 6 | 4 | 3
-# c - 8 | 4 | 3
-# d - 7 | 5 | 4
-# e - 4 | 3 | -
-# f - 9 | 5 | 4
-# g - 7 | 6 | -
-
-segments = {3:'7', 2:'1', 4:'4', 7:'8'}
-freq = 0
+easy_digits = 0
+output_total = 0
 
 for line in lines:
-    _input, output = line.split(' | ')
-    char_to_digit = {}
-    seg_freq = defaultdict(int)
-    for digit in _input.split():
-        for wire in digit:
-            seg_freq[wire] += 1
-    freq_seg = {v:k for k, v in seg_freq.items()}
-    for digit in output.split():
-        if len(digit) in segments:
-            freq += 1
-            char_to_digit[segments[len(digit)]] = digit
+    signals, digits = line.split(' | ')
 
-    import pudb;pu.db
-    one_seg = set(s['1'])
-    four_seg = set(s['4'])
-    seven_seg = set(s['7'])
-    eight_seg = set(s['8'])
+    five_segs = []
+    six_segs = []
+    for signal in signals.split():
+        if len(signal) == 2:
+            cf = set(signal)
+        elif len(signal) == 3:
+            acf = set(signal)
+        elif len(signal) == 4:
+            bcdf = set(signal)
+        elif len(signal) == 5:
+            five_segs.append(set(signal))
+        elif len(signal) == 6:
+            six_segs.append(set(signal))
+        elif len(signal) == 7:
+            abcdefg = set(signal)
 
-    # eg_seg = eight_seg - four_seg - seven_seg
-    bd_seg = four_seg - one_seg
-    e_seg = freq_seg[4]
-    g_seg = nine_seg - four_seg - seven_seg
+    a = acf - cf
+    bd = bcdf - cf
+    eg = abcdefg - bcdf - a
+    adg = reduce(set.intersection, five_segs)  # 2, 3, 5
+    b = bd - adg
+    abcefg = eg | acf | b
+    abfg = reduce(set.intersection, six_segs)  # 0, 6, 9
+    d = abcdefg - abcefg
+    g = adg - a - d
+    f = abfg - a - b - g
+    abdfg = a | b | d | f | g
+    ce = abcefg - abdfg
+    c = cf - f
+    e = ce - c
 
-    two_seg = None
-    three_seg = None
-    five_seg = None
-    six_seg = None
-    nine_seg = eight_seg - e_seg
+    zero = frozenset(a | b | c | e | f | g)
+    one = frozenset(c | f)
+    two = frozenset(a | c | d | e | g)
+    three = frozenset(a | c | d | f | g)
+    four = frozenset(b | c | d | f)
+    five = frozenset(a | b | d | f | g)
+    six = frozenset(a | b | d | e | f | g)
+    seven = frozenset(a | c | f)
+    eight = frozenset(a | b | c | d | e | f | g)
+    nine = frozenset(a | b | c | d | f | g)
 
-    a_seg = seven_seg - one_seg
-    b_seg = None
-    c_seg = None
-    d_seg = None
-    # e_seg = freq_seg[4]
-    f_seg = None
-    # g_seg = nine_seg - four_seg - seven_seg
+    seg_to_num = {
+        zero: '0',
+        one: '1',
+        two: '2',
+        three: '3',
+        four: '4',
+        five: '5',
+        six: '6',
+        seven: '7',
+        eight: '8',
+        nine: '9'
+    }
 
-print(freq)
+    output = ''
+    for digit in digits.split():
+        if len(digit) in {2, 3, 4, 7}:
+            easy_digits += 1
+
+        output += seg_to_num[frozenset(digit)]
+
+    output_total += int(output)
+
+print('Part 1:', easy_digits)
+print('Part 2:', output_total)
