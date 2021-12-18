b631de80efb8c5d33ed7a6ec8233f25dfbc83ca0
Derek Silbert <sjukledighet@gmail.com>
2021-12-18 18:30:57 -0500

    Day 17 full solution

--- p17.py
+++ p17.py
@@ -1,41 +1,33 @@
 import re
 
-from aocd import submit, data
+from aocd import  data
 
 
 def triangle_sum(n):
     return (n**2+n)//2
 
 
-# data = 'target area: x=20..30, y=-10..-5'
-
 x1, x2, y1, y2 = map(int, re.match(r'.+x=(\d+).+?(\d+).+?y=(-?\d+).+?(-?\d+)', data).groups())
 
-# x = 1
-# while triangle_sum(x) < x1:
-#     x += 1
-# min_x = x
+max_y = abs(y1+1)
+print('Part 1:', triangle_sum(max_y))
 
-# max_y = 0
-# y = 0
-# while True:
-#     y += 1
-#     i = 0
-#     stop = False
-#     while True:
-#         i += 1
-#         if y2 >= triangle_sum(y) - triangle_sum(i) >= y1:
-#             max_y = y
-#             break
-#         elif triangle_sum(y) - triangle_sum(i) < y1:
-#             stop = True
-#             break
+velocity_values = set()
 
-#     if stop:
-#         break
+for x in range(1, x2+1):
+    for y in range(y1, max_y+1):
+        vx = px = x
+        vy = py = y
+        while True:
+            if px > x2 or py < y1:
+                break
+            if x1 <= px <= x2 and y1 <= py <= y2:
+                velocity_values.add((x, y))
 
+            vx -= 1 if vx > 0 else 0
+            vy -= 1
 
+            px += vx
+            py += vy
 
-# print(min_x, max_y, triangle_sum(max_y))
-# 1653 too low
-print('Part 1:', triangle_sum(abs(y1+1)))
+print('Part 2:', len(velocity_values))
